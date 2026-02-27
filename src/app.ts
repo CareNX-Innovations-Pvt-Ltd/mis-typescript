13ef19ee6aad66ebadb4c21cca4e0588b50ec13c
Mansi Sable <mansi@carenx.com>
2026-02-27 18:42:50 +0530

    group user implementation

--- src/app.ts
+++ src/app.ts
@@ -8,16 +8,16 @@ app.use(
   cors({
     origin: [
       "http://localhost:8080",
-    //   "http://localhost:5173"
+      "https://mongo-compatibility.web.app",
+      "https://mis-typescript-1.onrender.com"
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
   })
 );
 
 app.use(express.json());
-
 app.use("/api", routes);
 
 export default app;
