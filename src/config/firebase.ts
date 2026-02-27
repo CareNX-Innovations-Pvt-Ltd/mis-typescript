7486e66ba5738ddd1a9207822d33e35688b90172
Mansi Sable <mansi@carenx.com>
2026-02-27 17:27:15 +0530

    group user implementation

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,7 +2,23 @@ import admin from "firebase-admin";
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
-const serviceAccount = require("../../serviceAccountKey.json");
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // Production (Render)
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+  // Fix newline issue in private key
+  serviceAccount.private_key =
+    serviceAccount.private_key.replace(/\\n/g, "\n");
+
+  console.log("Using ENV service account");
+} else {
+  // Local development
+  serviceAccount = require("../../serviceAccountKey.json");
+  console.log("Using local serviceAccountKey.json");
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
