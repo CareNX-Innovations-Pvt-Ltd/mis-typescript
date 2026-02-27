548d654c9617db999b47ec5a8c614b0639514a70
Mansi Sable <mansi@carenx.com>
2026-02-27 17:47:42 +0530

    group user implementation

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,23 @@ import express from "express";
 import routes from "./routes.js";
 
 const app = express();
-app.use(cors()); 
 
-app.use(express.json()); // also re-enable this
+app.use(
+  cors({
+    origin: [
+      "http://localhost:8080",
+    //   "http://localhost:5173"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+  })
+);
+
+app.options("*", cors());
+
+app.use(express.json());
 
 app.use("/api", routes);
 
-export default app; 
+export default app;
